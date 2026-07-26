d5b293ff736ae763ddd1d3b9c03ff3e12220bbaf
MuhammadRasoul Zahmatkesh <141241769+RasoulZahmatkesh@users.noreply.github.com>
2026-07-26 08:04:34 +0000

    Delete indicators.py

--- indicators.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import ta
-
-def add_indicators(df):
-    df['rsi'] = ta.momentum.RSIIndicator(df['close']).rsi()
-    df['ema_fast'] = ta.trend.EMAIndicator(df['close'], 12).ema_indicator()
-    df['ema_slow'] = ta.trend.EMAIndicator(df['close'], 26).ema_indicator()
-    df['macd'] = ta.trend.MACD(df['close']).macd()
-    return df
